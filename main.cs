f488f9b54402e4914a8268201a88921246b5d6af
Rayane Froura <rayaneamazighe02@gmail.com>
2025-03-31 15:02:45 -0400

    Update main.cs

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Affichage d'un message à l'écran
+        Console.WriteLine("Hello, GitHub! Je m'appelle Rayane Froura");
+    }
+}
